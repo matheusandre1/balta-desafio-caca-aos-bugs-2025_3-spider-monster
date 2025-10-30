fd0802c21607ece20f011522553b08b2e19e1763
Matheus Andre <matheusandr2@gmail.com>
2025-10-30 15:58:00 -0300

    feat: library infra

---
+++ src/BugStore.Infrastructure/Context/AppContextDbFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BugStore.Infra.Context;
+public class AppContextDbFactory : IDesignTimeDbContextFactory<AppContextDb>
+{
+    public AppContextDb CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppContextDb>();
+
+        var connection = Environment.GetEnvironmentVariable("ConnectionStrings__Default") 
+            ?? "Data Source=bugstore.db";
+
+        optionsBuilder.UseSqlite(connection);
+        optionsBuilder.UseModel(AppContextDbModel.Instance);
+
+        return new AppContextDb(optionsBuilder.Options);
+    }
+}
